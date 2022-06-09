44b677d81b136cd1700afe2e5700a4175ee1e322
rladbghks13 <rladbghks13@naver.com>
2022-06-09 17:27:32 +0900

    2022-06-09

--- app/src/main/java/com/example/sunhania/Fragment_todo_addAlarm.java
+++ app/src/main/java/com/example/sunhania/Fragment_todo_addAlarm.java
@@ -41,8 +41,15 @@ public class Fragment_todo_addAlarm extends Fragment {
     CheckBox checkBox_allday;
     protected int year, month, day, hour, minute;
     Calendar today;
-    private String upload_title,upload_startdate,upload_starttime,upload_enddate,upload_endtime,upload_alarmContent,upload_repeatFLAG,upload_alarmFLAG;
-
+    private String upload_title;
+    private String upload_startdate;
+    private String upload_starttime;
+    private String upload_enddate;
+    private String upload_endtime;
+    private String upload_alarmContent;
+    private String upload_repeatFLAG;
+    private String upload_alarmFLAG;
+    private int[] startdate, enddate;
 
 
     @Nullable
@@ -53,7 +60,8 @@ public class Fragment_todo_addAlarm extends Fragment {
         BackPressEvent();
         fragment_todo = new Fragment_todo();
 
-
+        startdate = new int[5];
+        enddate = new int[5];
         button_addAlarmExit = view.findViewById(R.id.btn_addAlarm_exit);
         button_addAlarm = view.findViewById(R.id.btn_addAlarm);
         add_title = view.findViewById(R.id.edittext_alarmtitle);
@@ -68,7 +76,7 @@ public class Fragment_todo_addAlarm extends Fragment {
 
         checkBox_allday.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view) { //하루종일 체크박스 선택했을시 종료시간 빈칸, 입력불가능하게 바꿈
                 if (checkBox_allday.isChecked()) {
                     add_starttime.setHint("하루종일");
                     add_starttime.setEnabled(false);
@@ -90,14 +98,32 @@ public class Fragment_todo_addAlarm extends Fragment {
         /**
          * Datepicker, Timepicker selectListener
          */
-        button_addAlarm.setOnClickListener(new View.OnClickListener() {
+        button_addAlarm.setOnClickListener(new View.OnClickListener() { //우측 상단 체크모양 눌러서 등록
             @Override
             public void onClick(View view) {
-                if (add_title.length() == 0) {
-
+                if (add_title.length() == 0) { //일정 제목에 아무것도 없으면 "제목 없음"으로 등록
+                    upload_title = "제목 없음";
                 } else {
-
+                    upload_title = String.valueOf(add_title.getText());
                 }
+
+                if (add_content.length() == 0) {
+                    upload_alarmContent = "내용 없음";
+                } else {
+                    upload_alarmContent = String.valueOf(add_content.getText());
+                }
+
+                if (!checkBox_allday.isChecked()) {
+                    if (add_startdate.length() == 0 || add_starttime.length() == 0 || add_enddate.length() == 0 || add_endtime.length() == 0) {
+                        Toast.makeText(getContext(), "날짜, 시간을 입력해주세요", Toast.LENGTH_SHORT).show();
+                    } else {
+                        if ((clacTime(startdate, enddate) == true)){
+
+                        }
+
+                    }
+                }
+
             }
         });
         add_startdate.setOnClickListener(new View.OnClickListener() {
@@ -154,7 +180,7 @@ public class Fragment_todo_addAlarm extends Fragment {
                                     @Override
                                     public void onClick(DialogInterface dialogInterface, int i) {
                                         upload_repeatFLAG = items1[i];
-                                        Log.i("test",items1[i]);
+                                        Log.i("test", items1[i]);
                                     }
                                 }).show();
                                 break;
@@ -200,6 +226,9 @@ public class Fragment_todo_addAlarm extends Fragment {
         public void onDateSet(DatePicker datePicker, int year, int month, int day) {
             Log.i("date", year + " " + month + " " + day);
             add_startdate.setText(year + "년 " + month + "월 " + day + "일");
+            startdate[0] = year;
+            startdate[1] = month;
+            startdate[2] = day;
         }
     };
     private DatePickerDialog.OnDateSetListener end_dateSetlistener = new DatePickerDialog.OnDateSetListener() {
@@ -207,7 +236,9 @@ public class Fragment_todo_addAlarm extends Fragment {
         public void onDateSet(DatePicker datePicker, int year, int month, int day) {
             Log.i("date", year + " " + month + " " + day);
             add_enddate.setText(year + "년 " + month + "월 " + day + "일");
-
+            enddate[0] = year;
+            enddate[1] = month;
+            enddate[2] = day;
         }
     };
 
@@ -215,6 +246,8 @@ public class Fragment_todo_addAlarm extends Fragment {
         @Override
         public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
             String mhour, mminute, ampm;
+            startdate[3] = hourOfDay;
+            startdate[4] = minute;
             String[] timearray = convertTime(hourOfDay, minute);
             mhour = timearray[0];
             mminute = timearray[1];
@@ -226,11 +259,13 @@ public class Fragment_todo_addAlarm extends Fragment {
         @Override
         public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
             String mhour, mminute, ampm;
+            enddate[3] = hourOfDay;
+            enddate[4] = minute;
             String[] timearray = convertTime(hourOfDay, minute);
             mhour = timearray[0];
             mminute = timearray[1];
             ampm = timearray[2];
-            add_starttime.setText(ampm + " " + mhour + "시 " + mminute + "분");
+            add_endtime.setText(ampm + " " + mhour + "시 " + mminute + "분");
         }
     }; //datepicker, timepicker 세팅
 
@@ -257,7 +292,7 @@ public class Fragment_todo_addAlarm extends Fragment {
     }
 
 
-    private String[] convertTime(int hourOfDay, int minute) {
+    private String[] convertTime(int hourOfDay, int minute) { //시간 선택했을때 텍스트뷰에 출력하기위해 변환
         String[] converTime = new String[3];
         if (hourOfDay > 13) {
             converTime[0] = String.valueOf(hourOfDay - 12);
@@ -273,14 +308,38 @@ public class Fragment_todo_addAlarm extends Fragment {
         }
         return converTime;
     }
-    private String test(int year, int month, int day){
+
+    private String test(int year, int month, int day) {  //반복일정 계산 테스트
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
         Calendar today = Calendar.getInstance();
-        today.set(year,month,day);
-        today.add(Calendar.YEAR,1);
-        Log.i("test",simpleDateFormat.format(today.getTime()));
+        today.set(year, month, day);
+        today.add(Calendar.YEAR, 1);
+        for (int i = 1; i <= 30; i++) {
+            today.add(Calendar.DAY_OF_MONTH, 1);
+            Log.i("test", simpleDateFormat.format(today.getTime()));
+        }
 
         return null;
+    }
+
+    private boolean clacTime(int[] startdate, int[] enddate) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar start = Calendar.getInstance();
+        Calendar end = Calendar.getInstance();
+        start.set(startdate[0], startdate[1], startdate[2], startdate[3], startdate[4]);
+        end.set(enddate[0], enddate[1], enddate[2], enddate[3], enddate[4]);
+        Log.i("test", simpleDateFormat.format((start.getTime())));
+        Log.i("test", simpleDateFormat.format((end.getTime())));
+
+        long today = start.getTimeInMillis();
+        long dday = end.getTimeInMillis();
+
+        long count = (today - dday);
+        if (count <= 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 
